49fc48838dd038fa9638e779ea6551d120c29a6d
Sandra Smirnova <sandra.smirnova@ymail.com>
2019-11-20 18:57:53 +0200

    new prectice

---
+++ Lecture9/src/com/aaccenture/lecture13/Practice.java
@@ -0,0 +1,13 @@
+package com.aaccenture.lecture13;
+
+public class Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
