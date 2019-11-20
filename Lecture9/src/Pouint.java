01badf0220800aa7db4c8b6bf7dfe386a5da607a
Sandra Smirnova <sandra.smirnova@ymail.com>
2019-11-20 18:51:55 +0200

    new class

---
+++ Lecture9/src/Pouint.java
@@ -0,0 +1,25 @@
+
+class Point{
+	int x;
+	int y;
+	
+	
+	
+	public String toString(){
+		return "x" + this.x + "y" + this.y;
+	}
+}
+
+
+public class Pouint {
+
+	 
+	public static void main(String[] args) {
+		Point startPoint = new Point();
+		startPoint.x = 0;
+		startPoint.y = 0;
+		
+
+	}
+
+}
